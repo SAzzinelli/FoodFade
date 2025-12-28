599b89a74c0e9971f1f19fbdd3fdc5d4f1923102
Simone Azzinelli <simone.azzinelli@labafirenze.coim>
2025-12-28 22:04:55 +0100

    Initial Commit

---
+++ FoodFade/FoodFadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFadeApp.swift
+//  FoodFade
+//
+//  Created by Simone Azzinelli on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodFadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
